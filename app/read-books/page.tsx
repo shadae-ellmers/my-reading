d3aed6528940d0fcb285d8ed4e180ccbf74adba6
Shadae <shadaejadeellmers@gmail.com>
2023-07-14 23:03:34 +1200

    read page set up and styling

--- app/read-books/page.tsx
+++ app/read-books/page.tsx
@@ -1,10 +1,73 @@
 import Link from 'next/link'
+import prisma from '../../prisma/client'
 
-export default function Page() {
+export default async function Page() {
+  const allReadBooks = await prisma.readBooks.findMany()
+
   return (
-    <>
-      <h2>Books I have read</h2>
-      <Link href="/read-books/add-read">Add books</Link>
-    </>
+    <div className="text-2xl leading-loose">
+      <div className="flex flex-row justify-between">
+        <h2 className="underline text-3xl font-extrabold pr-10">
+          Books I&apos;ve Read
+        </h2>
+        <a
+          href="/read-books/add-read"
+          className="relative inline-block text-lg group"
+        >
+          <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-fern transition-colors duration-300 ease-out border-2 border-fern rounded-lg group-hover:text-apricot">
+            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-apricot"></span>
+            <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-fern group-hover:-rotate-180 ease"></span>
+            <span className="relative">Add Books</span>
+          </span>
+          <span
+            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-fern rounded-lg group-hover:mb-0 group-hover:mr-0"
+            data-rounded="rounded-lg"
+          ></span>
+        </a>
+      </div>
+      <div>
+        {allReadBooks.map((book) => (
+          <div key={book.id} className="flex flex-row py-3">
+            {/* <img src={book.cover} alt="book cover" className="block pr-5" /> */}
+            <div className="flex flex-col justify-evenly">
+              <h3 className="font-extrabold">{book.title}</h3>
+              <p className="text-xl">{book.author}</p>
+              <div className="flex flex-row py-3">
+                <a
+                  href="#_"
+                  // onClick={() => clickHandler(book)}
+                  className="relative inline-block text-lg group mr-1"
+                >
+                  <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-fern transition-colors duration-300 ease-out border-2 border-fern rounded-lg group-hover:text-apricot">
+                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-apricot"></span>
+                    <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-fern group-hover:-rotate-180 ease"></span>
+                    <span className="relative">Update Rating</span>
+                  </span>
+                  <span
+                    className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-fern rounded-lg group-hover:mb-0 group-hover:mr-0"
+                    data-rounded="rounded-lg"
+                  ></span>
+                </a>
+                <a
+                  href="#_"
+                  // onClick={() => deleteHandler(book.id)}
+                  className="relative inline-block text-lg group ml-1"
+                >
+                  <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-fern transition-colors duration-300 ease-out border-2 border-fern rounded-lg group-hover:text-apricot">
+                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-apricot"></span>
+                    <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-fern group-hover:-rotate-180 ease"></span>
+                    <span className="relative">Remove</span>
+                  </span>
+                  <span
+                    className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-fern rounded-lg group-hover:mb-0 group-hover:mr-0"
+                    data-rounded="rounded-lg"
+                  ></span>
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
