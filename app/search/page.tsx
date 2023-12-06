03aae04e747ece95758e6feeadc7afa592268c70
Shadae <shadaejadeellmers@gmail.com>
2023-12-06 20:52:29 +1300

    map api results on page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,12 +3,44 @@ import { getData } from '../api/route'
 
 export default async function Page() {
   const data = await getData()
-  const firstData = data.docs[0]
+  const dataArray = data.docs
 
   return (
     <section className="justify-start h-screen text-2xl leading-loose py-8 px-16">
       <BookSearch />
-      {/* <p>{firstData.title}</p> */}
+      {dataArray.length > 1 ? (
+        <div>
+          {dataArray.map((book: any) => (
+            <div key={book.key}>
+              <h2 className="font-extrabold">{book.title}</h2>
+              {book.author_name ? (
+                book.author_name.map((author: any) => (
+                  <div key={author}>
+                    <p className="text-xl">{author}</p>
+                  </div>
+                ))
+              ) : (
+                <></>
+              )}
+              {book.cover_edition_key ? (
+                <img
+                  src={
+                    `https://covers.openlibrary.org/b/OLID/` +
+                    book.cover_edition_key +
+                    `.jpg`
+                  }
+                  alt="book cover"
+                  width="200"
+                />
+              ) : (
+                <></>
+              )}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div></div>
+      )}
     </section>
   )
 }
