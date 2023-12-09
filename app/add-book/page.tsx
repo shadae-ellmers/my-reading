f7cd5a159fea5afd2fa64e68e4bbf7694f9fcccb
Shadae <shadaejadeellmers@gmail.com>
2023-12-09 22:23:25 +1300

    remove adding data via form

--- app/add-book/page.tsx
+++ app/add-book/page.tsx
@@ -1,11 +1,23 @@
-import { BookCategoryRadio } from '../../components/BookCategoryRadio'
+import { BookSearch } from '../../components/BookSearch'
 
 export default function Page() {
   return (
     <div className="min-h-screen">
-      <h1>Add A Book To Your Collection</h1>
-      <h2>Which category do you want to add to?</h2>
-      <BookCategoryRadio />
+      <div className="relative">
+        <img
+          src="studio.jpg"
+          alt="apartment animation at night"
+          className="w-full max-h-60 object-cover"
+        ></img>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-4 py-3">
+          <h1 className="text-mywhite font-semibold text-4xl">
+            Reach your reading goals
+          </h1>
+        </div>
+      </div>
+      <div className="text-2xl leading-loose py-8 px-16">
+        <BookSearch />
+      </div>
     </div>
   )
 }
